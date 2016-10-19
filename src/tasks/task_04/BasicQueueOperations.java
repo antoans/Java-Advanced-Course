e0cfdd45e8622389396166874093c592d52d3e97
antoans <totokn@abv.bg>
2016-10-19 19:00:27 +0300

    Add files via upload

--- src/tasks/task_04/BasicQueueOperations.java
+++ src/tasks/task_04/BasicQueueOperations.java
@@ -1,8 +1,6 @@
 package tasks.task_04;
 
 import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -11,57 +9,54 @@ public class BasicQueueOperations {
 	public BasicQueueOperations() {
 		
 		Queue<Integer> queue = new ArrayDeque<>();
-		PriorityQueue<Integer> prioQueue;
 		
 		int length; 
 		int removeCount;
 		int checkNum;
+		boolean checkNumFound = false;
 		int minNum = Integer.MAX_VALUE;
 		String[] numbers;
 		
 		Scanner sc = new Scanner(System.in);
 		
-		
 		length = sc.nextInt();
-		prioQueue = new PriorityQueue<Integer>(length, new IntegerComparator());
 		removeCount = sc.nextInt();
 		checkNum = sc.nextInt();
 		sc.nextLine();
 		numbers = sc.nextLine().split(" ");
+		sc.close();
+		
 		//add elements
-		for (int i = 0; i < numbers.length; i++) {
+		for (int i = 0; i < length; i++) {
 			int currNum = Integer.parseInt(numbers[i]);
 			queue.add(currNum);
-			
-			prioQueue.offer(currNum);
 		}
 		
 		//remove elements
-		for (int i = 0; i < numbers.length; i++) {
-			int removedNumber = queue.poll();
-
-			if (removedNumber == prioQueue.peek()) {
-				prioQueue.poll();
+		for (int i = 0; i < removeCount; i++) {
+			queue.poll();
+		}
+		
+		//find min and check numbers
+		if (queue.size() == 0) {
+			minNum = 0;
+		}
+		
+		while (queue.size() > 0 ) {
+			int currNum = queue.poll().intValue();
+			if (currNum == checkNum) {
+				checkNumFound = true;
+			}
+			
+			if (currNum < minNum) {
+				minNum = currNum;
 			}
 		}
 		
-		//
-	}
-	class IntegerComparator implements Comparator<Integer>{
-
-		@Override
-		public int compare(Integer i1, Integer i2) {
-
-			if(i1.intValue() == 70 || i2.intValue() == 70) {
-				System.out.println("kur");
-			}
-			
-			if (i1.intValue() > i2.intValue()) {
-				return 1;
-			} else if (i1 < i2){
-				return -1;
-			} 
-			return 0;
+		if (checkNumFound) {
+			System.out.println("true");
+		} else {
+			System.out.println(minNum);
 		}
 		
 	}
