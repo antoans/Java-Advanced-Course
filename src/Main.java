51b50b1d1626b980720d621e2277b2d21b8ae47c
antoans <totokn@abv.bg>
2016-10-25 18:26:17 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,29 @@ import tasks.task_01.*;
 import tasks.task_02.*;
 import tasks.task_03.*;
 import tasks.task_04.*;
+import tasks.task_05.*;
+import tasks.task_06.*;
+import tasks.task_07.*;
+import tasks.task_08.*;
+import tasks.task_09.*;
+import tasks.task_10.*;
+import tasks.task_11.*;
+import tasks.task_11.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//ReverseNumbers r = new ReverseNumbers();
-		//BasicStackOperations b = new BasicStackOperations();
-		//MaximumElement m = new MaximumElement();
-		BasicQueueOperations q = new BasicQueueOperations(); 
+//		new ReverseNumbers();
+//		new BasicStackOperations();
+//		new MaximumElement();
+//		new BasicQueueOperations();
+//		new SequenceWithQueue();
+//		new TruckTour();
+//		new BalancedParentheses();
+//		new RecursiveFibonacci();
+//		new StackFibonacci();
+//		new SimpleTextEditor();
+//		new PoisonousPlants();
 	}
 
 
